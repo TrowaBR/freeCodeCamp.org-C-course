e96affd419474470cf5ec536cfacc3955997e3c2
trowabr <raphaelcteodoro@hotmail.com>
2022-06-04 22:00:10 -0300

    Exercises from Protected Members lession

---
+++ 35.Protected Members/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "player.h"
+
+int main() {
+
+    Player player1("Basketball", "John", "Snow");
+
+    std::cout << "player1: " << player1 << std::endl;
+
+    return 0;
+}
---
+++ 35.Protected Members/person.cpp
@@ -0,0 +1,28 @@
+#include "person.h"
+//std::ostream& operator << (std::ostream& out, const Person& person);
+
+Person::Person(std::string& first_name_param, std::string& last_name_param)
+: m_first_name(first_name_param), m_last_name(last_name_param)
+{
+}
+
+// Getters
+std::string Person::get_first_name() const {
+    return m_first_name;
+}
+std::string Person::get_last_name() const {
+    return m_last_name;
+}
+
+// Setters
+void Person::set_first_name(std::string_view first_name_param) {
+    m_first_name = first_name_param;
+}
+void Person::set_last_name(std::string_view last_name_param) {
+    m_last_name = last_name_param;
+}
+
+std::ostream& operator<<(std::ostream& out, const Person& person) {
+    out << "Person: [ " << person.m_first_name << " " << person.m_last_name << " ]";
+    return out;
+}
